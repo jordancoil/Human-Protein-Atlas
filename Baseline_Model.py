32d186d94e91dcda7c78e5520af2a43a1fae4338
Raegal596 <45675420+Raegal596@users.noreply.github.com>
2018-12-06 17:33:42 -0800

    Added baseline model

---
+++ Baseline_Model.py
@@ -0,0 +1,519 @@
+# Time the execution, for benchmarking purposes. Begin by setting a checkpoint.
+import time
+start_time = time.time()
+
+#Define kernal settings
+class KernelSettings:
+    
+    def __init__(self, fit_baseline=False, fit_improved_baseline=False):
+        self.fit_baseline = fit_baseline
+        self.fit_improved_baseline = fit_improved_baseline
+
+kernelsettings = KernelSettings(fit_baseline=False, fit_improved_baseline=True)
+use_dropout=False
+#Import Libraries
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+#%matplotlib inline
+from PIL import Image
+from scipy.misc import imread
+
+import tensorflow as tf
+
+# could possibly remove these
+import os
+import warnings
+
+# Import the labels for the training set, and the corresponding 
+train_labels = pd.read_csv("train.csv")
+
+# Create a dictionary assigning labels to each of the 28 cell functions/locations
+label_names = {
+    0:  "Nucleoplasm",  
+    1:  "Nuclear membrane",   
+    2:  "Nucleoli",   
+    3:  "Nucleoli fibrillar center",   
+    4:  "Nuclear speckles",
+    5:  "Nuclear bodies",   
+    6:  "Endoplasmic reticulum",   
+    7:  "Golgi apparatus",   
+    8:  "Peroxisomes",   
+    9:  "Endosomes",   
+    10:  "Lysosomes",   
+    11:  "Intermediate filaments",   
+    12:  "Actin filaments",   
+    13:  "Focal adhesion sites",   
+    14:  "Microtubules",   
+    15:  "Microtubule ends",   
+    16:  "Cytokinetic bridge",   
+    17:  "Mitotic spindle",   
+    18:  "Microtubule organizing center",   
+    19:  "Centrosome",   
+    20:  "Lipid droplets",   
+    21:  "Plasma membrane",   
+    22:  "Cell junctions",   
+    23:  "Mitochondria",   
+    24:  "Aggresome",   
+    25:  "Cytosol",   
+    26:  "Cytoplasmic bodies",   
+    27:  "Rods & rings"
+}
+
+reverse_train_labels = dict((v,k) for k,v in label_names.items())
+
+# One hot encode the training data and correct responses
+def fill_targets(row):
+    row.Target = np.array(row.Target.split(" ")).astype(np.int)
+    for num in row.Target:
+        name = label_names[int(num)]
+        row.loc[name] = 1
+    return row
+
+for key in label_names.keys():
+    train_labels[label_names[key]] = 0
+    
+train_labels = train_labels.apply(fill_targets, axis=1)
+train_labels["number_of_targets"] = train_labels.drop(["Id", "Target"],axis=1).sum(axis=1)
+
+# Define functions to load the images
+train_path = "train/"
+
+def load_image(basepath, image_id):
+    images = np.zeros(shape=(4,512,512))
+    images[0,:,:] = imread(basepath + image_id + "_green" + ".png")
+    images[1,:,:] = imread(basepath + image_id + "_red" + ".png")
+    images[2,:,:] = imread(basepath + image_id + "_blue" + ".png")
+    images[3,:,:] = imread(basepath + image_id + "_yellow" + ".png")
+    return images
+
+# Could possibly remove this, it may just be for visualisation
+def make_image_row(image, subax, title):
+    subax[0].imshow(image[0], cmap="Greens")
+    subax[1].imshow(image[1], cmap="Reds")
+    subax[1].set_title("stained microtubules")
+    subax[2].imshow(image[2], cmap="Blues")
+    subax[2].set_title("stained nucleus")
+    subax[3].imshow(image[3], cmap="Oranges")
+    subax[3].set_title("stained endoplasmatic reticulum")
+    subax[0].set_title(title)
+    return subax
+
+def make_title(file_id):
+    file_targets = train_labels.loc[train_labels.Id==file_id, "Target"].values[0]
+    title = " - "
+    for n in file_targets:
+        title += label_names[n] + " - "
+    return title
+
+# This block of code is for visualising images which have particular proteins identified. Could possibly remove this
+class TargetGroupIterator:
+    
+    def __init__(self, target_names, batch_size, basepath):
+        self.target_names = target_names
+        self.target_list = [reverse_train_labels[key] for key in target_names]
+        self.batch_shape = (batch_size, 4, 512, 512)
+        self.basepath = basepath
+    
+    def find_matching_data_entries(self):
+        train_labels["check_col"] = train_labels.Target.apply(
+            lambda l: self.check_subset(l)
+        )
+        self.images_identifier = train_labels[train_labels.check_col==1].Id.values
+        train_labels.drop("check_col", axis=1, inplace=True)
+    
+    def check_subset(self, targets):
+        return np.where(set(targets).issubset(set(self.target_list)), 1, 0)
+    
+    def get_loader(self):
+        filenames = []
+        idx = 0
+        images = np.zeros(self.batch_shape)
+        for image_id in self.images_identifier:
+            images[idx,:,:,:] = load_image(self.basepath, image_id)
+            filenames.append(image_id)
+            idx += 1
+            if idx == self.batch_shape[0]:
+                yield filenames, images
+                filenames = []
+                images = np.zeros(self.batch_shape)
+                idx = 0
+        if idx > 0:
+            yield filenames, images
+
+# k-fold cross-validation
+from sklearn.model_selection import RepeatedKFold
+
+splitter = RepeatedKFold(n_splits=3, n_repeats=1, random_state=0)
+
+partitions = []
+
+for train_idx, test_idx in splitter.split(train_labels.index.values):
+    partition = {}
+    partition["train"] = train_labels.Id.values[train_idx]
+    partition["validation"] = train_labels.Id.values[test_idx]
+    partitions.append(partition)
+
+# Define the CNN parameters
+class ModelParameter:
+    
+    def __init__(self, basepath,
+                 num_classes=28,
+                 image_rows=512,
+                 image_cols=512,
+                 batch_size=200,
+                 n_channels=1,
+                 row_scale_factor=4,
+                 col_scale_factor=4,
+                 shuffle=False,
+                 n_epochs=1):
+        self.basepath = basepath
+        self.num_classes = num_classes
+        self.image_rows = image_rows
+        self.image_cols = image_cols
+        self.batch_size = batch_size
+        self.n_channels = n_channels
+        self.shuffle = shuffle
+        self.row_scale_factor = row_scale_factor
+        self.col_scale_factor = col_scale_factor
+        self.scaled_row_dim = np.int(self.image_rows / self.row_scale_factor)
+        self.scaled_col_dim = np.int(self.image_cols / self.col_scale_factor)
+        self.n_epochs = n_epochs
+
+parameter = ModelParameter(train_path)
+
+# Set up an image preprocessing class
+from skimage.transform import resize
+
+class ImagePreprocessor:
+    
+    def __init__(self, modelparameter):
+        self.parameter = modelparameter
+        self.basepath = self.parameter.basepath
+        self.scaled_row_dim = self.parameter.scaled_row_dim
+        self.scaled_col_dim = self.parameter.scaled_col_dim
+        self.n_channels = self.parameter.n_channels
+    
+    def preprocess(self, image):
+        image = self.resize(image)
+        image = self.reshape(image)
+        image = self.normalize(image)
+        return image
+    
+    def resize(self, image):
+        image = resize(image, (self.scaled_row_dim, self.scaled_col_dim), mode = 'constant', anti_aliasing = True)
+        return image
+    
+    def reshape(self, image):
+        image = np.reshape(image, (image.shape[0], image.shape[1], self.n_channels))
+        return image
+    
+    def normalize(self, image):
+        image /= 255 
+        return image
+    
+    def load_image(self, image_id):
+        image = np.zeros(shape=(512,512,4))
+        image[:,:,0] = imread(self.basepath + image_id + "_green" + ".png")
+        image[:,:,1] = imread(self.basepath + image_id + "_blue" + ".png")
+        image[:,:,2] = imread(self.basepath + image_id + "_red" + ".png")
+        image[:,:,3] = imread(self.basepath + image_id + "_yellow" + ".png")
+        return image[:,:,0:self.parameter.n_channels]
+
+preprocessor = ImagePreprocessor(parameter)
+
+# Data Generator
+import keras
+
+class DataGenerator(keras.utils.Sequence):
+    
+    def __init__(self, list_IDs, labels, modelparameter, imagepreprocessor):
+        self.params = modelparameter
+        self.labels = labels
+        self.list_IDs = list_IDs
+        self.dim = (self.params.scaled_row_dim, self.params.scaled_col_dim)
+        self.batch_size = self.params.batch_size
+        self.n_channels = self.params.n_channels
+        self.num_classes = self.params.num_classes
+        self.shuffle = self.params.shuffle
+        self.preprocessor = imagepreprocessor
+        self.on_epoch_end()
+    
+    def on_epoch_end(self):
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+    
+    def get_targets_per_image(self, identifier):
+        return self.labels.loc[self.labels.Id==identifier].drop(
+                ["Id", "Target", "number_of_targets"], axis=1).values
+            
+    def __data_generation(self, list_IDs_temp):
+        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        # Initialization
+        X = np.empty((self.batch_size, *self.dim, self.n_channels))
+        y = np.empty((self.batch_size, self.num_classes), dtype=int)
+        # Generate data
+        for i, identifier in enumerate(list_IDs_temp):
+            # Store sample
+            image = self.preprocessor.load_image(identifier)
+            image = self.preprocessor.preprocess(image)
+            X[i] = image
+            # Store class
+            y[i] = self.get_targets_per_image(identifier)
+        return X, y
+    
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
+    
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+        # Find list of IDs
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+        # Generate data
+        X, y = self.__data_generation(list_IDs_temp)
+        return X, y
+
+# Implementation of the F1 score
+import keras.backend as K
+
+def base_f1(y_true, y_pred):
+    y_pred = K.round(y_pred)
+    tp = K.sum(K.cast(y_true*y_pred, 'float'), axis=0)
+    tn = K.sum(K.cast((1-y_true)*(1-y_pred), 'float'), axis=0)
+    fp = K.sum(K.cast((1-y_true)*y_pred, 'float'), axis=0)
+    fn = K.sum(K.cast(y_true*(1-y_pred), 'float'), axis=0)
+
+    p = tp / (tp + fp + K.epsilon())
+    r = tp / (tp + fn + K.epsilon())
+
+    f1 = 2*p*r / (p+r+K.epsilon())
+    f1 = tf.where(tf.is_nan(f1), tf.zeros_like(f1), f1)
+    return f1
+
+def f1_min(y_true, y_pred):
+    f1 = base_f1(y_true, y_pred)
+    return K.min(f1)
+
+def f1_max(y_true, y_pred):
+    f1 = base_f1(y_true, y_pred)
+    return K.max(f1)
+
+def f1_mean(y_true, y_pred):
+    f1 = base_f1(y_true, y_pred)
+    return K.mean(f1)
+
+def f1_std(y_true, y_pred):
+    f1 = base_f1(y_true, y_pred)
+    return K.std(f1)
+    
+# Predict Generator
+class PredictGenerator:
+    
+    def __init__(self, predict_Ids, imagepreprocessor, predict_path):
+        self.preprocessor = imagepreprocessor
+        self.preprocessor.basepath = predict_path
+        self.identifiers = predict_Ids
+    
+    def predict(self, model):
+        y = np.empty(shape=(len(self.identifiers), self.preprocessor.parameter.num_classes))
+        for n in range(len(self.identifiers)):
+            image = self.preprocessor.load_image(self.identifiers[n])
+            image = self.preprocessor.preprocess(image)
+            image = image.reshape((1, *image.shape))
+            y[n] = model.predict(image)
+        return y
+
+# Build the baseline model
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.losses import binary_crossentropy
+from keras.optimizers import Adadelta
+from keras.models import load_model
+
+
+class BaseLineModel:
+    
+    def __init__(self, modelparameter):
+        self.params = modelparameter
+        self.num_classes = self.params.num_classes
+        self.img_rows = self.params.scaled_row_dim
+        self.img_cols = self.params.scaled_col_dim
+        self.n_channels = self.params.n_channels
+        self.input_shape = (self.img_rows, self.img_cols, self.n_channels)
+        self.my_metrics = ['accuracy']
+    
+    def build_model(self):
+        self.model = Sequential()
+        self.model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', input_shape=self.input_shape))
+        self.model.add(Conv2D(32, (3, 3), activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.25))
+        self.model.add(Flatten())
+        self.model.add(Dense(64, activation='relu'))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(self.num_classes, activation='sigmoid'))
+    
+    def compile_model(self):
+        self.model.compile(loss=keras.losses.binary_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=self.my_metrics)
+    
+    def set_generators(self, train_generator, validation_generator):
+        self.training_generator = train_generator
+        self.validation_generator = validation_generator
+    
+    def learn(self):
+        return self.model.fit_generator(generator=self.training_generator,
+                    validation_data=self.validation_generator,
+                    epochs=self.params.n_epochs, 
+                    use_multiprocessing=True,
+                    workers=8)
+    
+    def score(self):
+        return self.model.evaluate_generator(generator=self.validation_generator,
+                                      use_multiprocessing=True, 
+                                      workers=8)
+    
+    def predict(self, predict_generator):
+        y = predict_generator.predict(self.model)
+        return y
+    
+    def save(self, modeloutputpath):
+        self.model.save(modeloutputpath)
+    
+    def load(self, modelinputpath):
+        self.model = load_model(modelinputpath)
+
+# Use the first partition to train the model
+partition = partitions[0]
+labels = train_labels
+
+training_generator = DataGenerator(partition['train'], labels, parameter, preprocessor)
+validation_generator = DataGenerator(partition['validation'], labels, parameter, preprocessor)
+predict_generator = PredictGenerator(partition['validation'], preprocessor, train_path)
+
+# Run computation and store results as csv
+if kernelsettings.fit_baseline == True:
+    model = BaseLineModel(parameter)
+    model.build_model()
+    model.compile_model()
+    model.set_generators(training_generator, validation_generator)
+    history = model.learn()
+    #model.save("baseline_model.h5")
+    proba_predictions = model.predict(predict_generator)
+    baseline_proba_predictions = pd.DataFrame(proba_predictions, columns=train_labels.drop(
+        ["Target", "number_of_targets", "Id"], axis=1).columns)
+    baseline_proba_predictions.to_csv("baseline_predictions.csv")
+# If you already have done a baseline fit once, 
+# you can load predictions as csv and further fitting is not neccessary:
+else:
+    baseline_proba_predictions = pd.read_csv("baseline_predictions.csv", index_col=0)
+
+
+# Implement loss callback
+class TrackHistory(keras.callbacks.Callback):
+    
+    def on_train_begin(self, logs={}):
+        self.losses = []
+
+    def on_batch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+
+# Implement an improved data generator
+wishlist = ["Nucleoplasm", "Cytosol", "Plasma membrane"]
+
+class ImprovedDataGenerator(DataGenerator):
+    
+    # in contrast to the base DataGenerator we add a target wishlist to init
+    def __init__(self, list_IDs, labels, modelparameter, imagepreprocessor, target_wishlist):
+        super().__init__(list_IDs, labels, modelparameter, imagepreprocessor)
+        self.target_wishlist = target_wishlist
+    
+    def get_targets_per_image(self, identifier):
+        return self.labels.loc[self.labels.Id==identifier][self.target_wishlist].values
+        
+# Implement an improved version of the model
+class ImprovedModel(BaseLineModel):
+    
+    def __init__(self, modelparameter,
+                 use_dropout = True,
+                 my_metrics=[f1_mean, f1_std, f1_min, f1_max]):
+        
+        super().__init__(modelparameter)
+        self.my_metrics = my_metrics
+        self.use_dropout = use_dropout
+        
+    def learn(self):
+        self.history = TrackHistory()
+        return self.model.fit_generator(generator=self.training_generator,
+                    validation_data=self.validation_generator,
+                    epochs=self.params.n_epochs, 
+                    use_multiprocessing=True,
+                    workers=8,
+                    callbacks = [self.history])
+    
+    def build_model(self):
+        self.model = Sequential()
+        self.model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', input_shape=self.input_shape))
+        self.model.add(Conv2D(32, (3, 3), activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        if self.use_dropout:
+            self.model.add(Dropout(0.25))
+        self.model.add(Flatten())
+        self.model.add(Dense(64, activation='relu'))
+        if self.use_dropout:
+            self.model.add(Dropout(0.5))
+        self.model.add(Dense(self.num_classes, activation='sigmoid'))
+
+# Reset the model parameters
+parameter = ModelParameter(train_path, num_classes=len(wishlist), n_epochs=5, batch_size=64)
+preprocessor = ImagePreprocessor(parameter)
+labels = train_labels
+
+# Set up the model
+training_generator = ImprovedDataGenerator(partition['train'], labels,
+                                           parameter, preprocessor, wishlist)
+validation_generator = ImprovedDataGenerator(partition['validation'], labels,
+                                             parameter, preprocessor, wishlist)
+predict_generator = PredictGenerator(partition['validation'], preprocessor, train_path)
+
+# Run computation and store results as csv
+if kernelsettings.fit_improved_baseline == True:
+    model = ImprovedModel(parameter, use_dropout=use_dropout)
+    model.build_model()
+    model.compile_model()
+    model.set_generators(training_generator, validation_generator)
+    epoch_history = model.learn()
+    proba_predictions = model.predict(predict_generator)
+    model.save("improved_model.h5")
+    improved_proba_predictions = pd.DataFrame(proba_predictions, columns=wishlist)
+    improved_proba_predictions.to_csv("improved_predictions.csv")
+# If you already have done a baseline fit once, 
+# you can load predictions as csv and further fitting is not neccessary:
+else:
+    improved_proba_predictions = pd.read_csv("../input/protein-atlas-eab-predictions/improved_predictions.csv", index_col=0)
+    model = load_model("improved_model.h5")
+
+if kernelsettings.fit_improved_baseline == True:
+    print(epoch_history.history.keys())
+    fig, ax = plt.subplots(2,1,figsize=(20,10))
+    ax[0].plot(model.history.losses, color="Red")
+    ax[0].set_xlabel("Update step after one batch")
+    ax[0].set_ylabel("Train loss")
+    ax[0].set_title("Loss evolution batch per batch")
+    ax[1].plot(epoch_history.history["val_loss"], color="Green")
+    ax[1].plot(epoch_history.history["loss"])
+    ax[1].set_title("Loss evolution per epoch")
+    ax[1].set_xlabel("Update step after one epoch")
+    ax[1].set_ylabel("Loss")
+    fig.savefig("losses_improved_model", format="eps")
+
+# Determine the total execution time, and print this
+print(time.time() - start_time)
